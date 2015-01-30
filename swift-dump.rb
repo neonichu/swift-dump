21f6dded976fb4fd21e1c922290c550a923e7053
Boris Bügling <boris@icculus.org>
2015-01-30 23:09:50 +0100

    Implemented support for variables.

--- swift-dump.rb
+++ swift-dump.rb
@@ -39,6 +39,30 @@ DUMP
 				c_name = class_name(sclass)
 				dump << "class #{c_name.split('.')[1]} {\n"
 
+				variables(sclass).each do |var|
+					v = Swift::demangle_symbol(var).split('.')[2]
+
+					mangled_type = var.gsub(/^#{sclass}#{GETTER}#{v}/, '')
+					type = Swift::demangle_symbol(var).split(' : ')[1]
+					type = type.gsub(/Swift\./, '')
+
+					init = ''
+					if has_direct_field(sclass, v, mangled_type)
+						init = " = #{Swift::default_value(type)} "
+					end
+
+					declaration = 'var'
+					unless has_setter(sclass, v, mangled_type)
+						if has_direct_field(sclass, v, mangled_type)
+							declaration = 'let'
+						else
+							init = " { return #{Swift::default_value(type)} } "
+						end
+					end
+
+					dump << "#{declaration} #{v}: #{type}#{init}\n"
+				end
+
 				functions(sclass).each do |func|
 					f = Swift::demangle_symbol(func).gsub(/ \(#{c_name}\)/, '')
 					f = f.gsub(/^#{c_name}\./, '')
@@ -63,7 +87,11 @@ DUMP
 
 		CLASS = '__TFC'
 		DEALLOC = 'D'
+		DIRECT = '__TWvdvC'
 		FUNC = '3'
+		GETTER = 'g1'
+		MATERIALIZE = 'm1'
+		SETTER = 's1'
 		VOID_FUNC = '7'
 
 		def classes
@@ -74,12 +102,32 @@ DUMP
 			Swift::demangle_symbol(mangled_class_prefix + DEALLOC).split('.__')[0]
 		end
 
+		def has_direct_field(mangled_class_prefix, variable_name, mangled_type)
+			prefix = mangled_class_prefix.gsub(CLASS, DIRECT)
+			expected_sym = "#{prefix}1#{variable_name}#{mangled_type}"
+			@symbols.select{ |sym| sym == expected_sym }.length > 0
+		end
+
 		def functions(mangled_class_prefix)
 			relevant_symbols.select { |sym| sym[/^#{mangled_class_prefix}(#{FUNC}|#{VOID_FUNC})/] }
 		end
 
+		def has_materialize(mangled_class_prefix, variable_name, mangled_type)
+			expected_sym = "#{mangled_class_prefix}#{MATERIALIZE}#{variable_name}#{mangled_type}"
+			relevant_symbols.select { |sym| sym == expected_sym }.length > 0
+		end
+
+		def has_setter(mangled_class_prefix, variable_name, mangled_type)
+			expected_sym = "#{mangled_class_prefix}#{SETTER}#{variable_name}#{mangled_type}"
+			relevant_symbols.select { |sym| sym == expected_sym }.length > 0
+		end
+
 		def relevant_symbols
 			@symbols.select { |sym| sym[/^#{CLASS}/] }
+		end
+
+		def variables(mangled_class_prefix)
+			relevant_symbols.select { |sym| sym[/^#{mangled_class_prefix}#{GETTER}/] }
 		end
 	end
 end
